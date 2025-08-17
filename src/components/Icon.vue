626f3ff9817fc896e2143a82a380a59d3e4db692
MarkusJLechner <markus@hellocash.at>
2025-08-17 15:28:43 +0200

    wip

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,21 +1,125 @@
 <template>
-  <i :class="['relative', cls, icon]" :style="cstyles" />
+  <i :class="['relative', cls, icon]" :style="rootStyle" />
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
   
 </template>
 
 <script lang="ts" setup>
 import { computed } from 'vue'
 
-const props = defineProps<{ icon?: string; size?: string; class?: string; style?: string }>()
+const props = withDefaults(defineProps<{ icon?: string; size?: string; class?: string }>(), {
+  size: '3rem',
+  icon: '',
+  class: '',
+})
 
 const cls = computed(() => props.class || '')
-const cstyles = computed(() => {
-  const size = props.size ?? '3rem'
-  const styles: Record<string, string> = { 'font-size': size }
-  const base = Object.entries(styles)
-    .map(([k, v]) => `${k}:${v}`)
-    .join(';')
-  return `${base}; ${props.style ?? ''}`
-})
+const rootStyle = computed(() => ({ fontSize: props.size }))
 </script>
-
