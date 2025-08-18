463331177621579647a43bc245a8f02c0243a7cb
MarkusJLechner <markus@hellocash.at>
2025-08-18 14:29:10 +0200

    update layout in App component, add spacer div for improved structure and styling consistency

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="App h-[100dvh] flex flex-col">
     <Header />
-    <div class="mt-16 bg-gray-200 flex-grow flex flex-col p-2 font-sans">
+    <div class='w-full min-h-16'></div>
+    <div class="bg-gray-200 flex-grow flex flex-col p-2 font-sans">
       <RouterView />
     </div>
   </div>
